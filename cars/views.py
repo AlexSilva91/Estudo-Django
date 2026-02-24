63333ef52dccf12e6e4669e446a4a085bfa69180
AlexSilva91 <alexsilvaalves17@outlook.com>
2026-02-24 00:17:18 -0300

    refactor: add templates base no core da aplicação e configurado e apontado no DIRS o caminho do template
    refactor: nomeado a rota cars/
    feat: add template cars.html e base.html
    refactor: car_view para aplicar filtros por model e ordenação

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+def cars_view(request):
+    cars = Car.objects.all().order_by('brand')
+    search = request.GET.get('search')
+    
+    """ Valida se existe parâmetro de busca, caso exista, filtra os carros pelo modelo """
+    if search:
+        cars = cars.filter(model__icontains=search)
+        
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': cars}
+    )
