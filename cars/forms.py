f7ae906a0eead47983c185a0054d2fe1c29cd19a
AlexSilva91 <alexsilvaalves17@outlook.com>
2026-02-24 22:09:13 -0300

    feat: adicionando validações com ModelForms

--- cars/forms.py
+++ cars/forms.py
@@ -6,4 +6,23 @@ class CarModelForm(forms.ModelForm):
         model = Car
         fields = '__all__'
         
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
         
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve ser maior que R$ 20.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possível cadastrar veículo de fabricação anterior a 1975!')
+        return factory_year
+    
+    def clean_photo(self):
+        photo = self.cleaned_data.get('photo')
+        
+        if photo is None:
+            self.add_error('photo', 'Não é possível cadastrar carro sem foto!')
+        return photo
